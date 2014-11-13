a0976ee26ba265cd54e16e38243e29dfba4a031a
大雄 <hsupangfei1989@gmail.com>
2014-11-13 18:23:29 +0800

    学习thrift时做的thrift的例子

---
+++ thrift-example/src/main/java/org/seava/thrift_example/App.java
@@ -0,0 +1,13 @@
+package org.seava.thrift_example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ thrift-example/src/main/java/org/seava/thrift_example/thrift/AsynCallback.java
@@ -0,0 +1,23 @@
+package org.seava.thrift_example.thrift;
+
+import org.apache.thrift.async.AsyncMethodCallback;
+
+/**
+ * @author WaterHsu@xiu8.com
+ * @version 2014年11月13日
+ */
+public class AsynCallback implements AsyncMethodCallback<ThriftService> {
+
+	@Override
+	public void onComplete(ThriftService arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onError(Exception arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
